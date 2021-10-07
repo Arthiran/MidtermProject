b74908120f21bc92d4d76eedca2d5de75f8aafc8
Arthiran <arthiran04@gmail.com>
2021-10-07 14:19:54 -0400

    Initial commit

---
+++ PluginProject/UnityPlugin/UnityPlugin/GameObject.cpp
@@ -0,0 +1,30 @@
+// Arthiran Sivarajah - 100660300 : GameObject.cpp
+
+#include "GameObject.h"
+
+GameObject::GameObject()
+{
+	SetID();
+	SetPosition();
+}
+
+int GameObject::GetID() const
+{
+	return m_id;
+}
+
+void GameObject::SetID(const int id)
+{
+	m_id = id;
+}
+
+Vector2D GameObject::GetPosition() const
+{
+	return m_position;
+}
+
+void GameObject::SetPosition(const float x, const float y)
+{
+	m_position.x = x;
+	m_position.y = y;
+}
---
+++ PluginProject/UnityPlugin/UnityPlugin/Wrapper.cpp
@@ -0,0 +1,26 @@
+// Arthiran Sivarajah - 100660300 : Wrapper.cpp
+
+#include "Wrapper.h"
+#include "GameObject.h"
+
+GameObject gameObject;
+
+PLUGIN_API int GetID()
+{
+	return gameObject.GetID();
+}
+
+PLUGIN_API void SetID(int id)
+{
+	gameObject.SetID(id);
+}
+
+PLUGIN_API Vector2D GetPosition()
+{
+	return gameObject.GetPosition();
+}
+
+PLUGIN_API void SetPosition(float x, float y)
+{
+	gameObject.SetPosition(x, y);
+}
